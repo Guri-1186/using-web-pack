3ab985a02ea0f360cbe06e59b21730c9b9e98ba2
Guri Jikia <jikiaguranda@gmail.com>
2023-07-24 12:29:32 +0400

    perform task

---
+++ src/join-us.js
@@ -0,0 +1,62 @@
+
+export const JoinUsSection = {
+  htmlElem: `
+    <section class="app-section app-section--image-join">
+      <h2 class="app-title">Join Our Program</h2>
+      <h3 class="app-subtitle">Sed do eiusmod tempor incididunt <br /> ut labore et dolore magna aliqua.<br /></h3>
+      
+      <div class="email-subscribe-container">
+        <input type="email" id="email" name="email" placeholder="Email">
+        <button class="app-section__button app-section__button--subscribe">SUBSCRIBE</button>
+      </div>
+    </section>
+  `,
+
+
+  
+  render: function () {
+    const footer = document.querySelector('footer');
+    footer.insertAdjacentHTML('beforebegin', this.htmlElem);
+
+    const submitButton = document.querySelector('button.app-section__button--subscribe');
+    submitButton.addEventListener('click', function (e) {
+      e.preventDefault();
+      console.log(document.querySelector("#email").value);
+    });
+  },
+};
+  
+
+
+class Section {
+  constructor(htmlElem) {
+    this.htmlElem = htmlElem;
+  }
+
+ 
+  remove() {
+    const section = document.querySelector('.app-section--image-join');
+    if (section) {
+      section.remove();
+    }
+  }
+}
+
+class SectionCreatorFactory {
+  createSection(type) {
+    switch (type) {
+      case 'standard':
+        return new Section(JoinUsSection.htmlElem);
+
+      case 'advanced':
+        return new Section(
+          JoinUsSection.htmlElem.replace('Join Our Program', 'Join Our Advanced Program')
+                                .replace('SUBSCRIBE', 'Subscribe to Advanced Program')
+        );
+  }
+}
+}
+
+export { SectionCreatorFactory };
+
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import JoinUsSection from './join-us.js';
 
-document.addEventListener('DOMContentLoaded', function() {
-  JoinUsSection.render();
+
+
+ 
+import { JoinUsSection } from './join-us.js';
+import { SectionCreatorFactory } from './join-us.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const sectionCreator = new SectionCreatorFactory();
+
+  JoinUsSection.render(); 
+
+  // const StandardSection = sectionCreator.createSection('standard');
+  // StandardSection.render(); // Render the StandardSection.
+
+  // const AdvancedSection = sectionCreator.createSection('advanced');
+  // AdvancedSection.render(); // Render the AdvancedSection.
 });
