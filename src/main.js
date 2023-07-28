75f7be283589d9022a8bf288a05d15d1a7d9ff1b
cilgaiscan <cilgaiscan@gmail.com>
2023-07-28 19:39:42 +0200

    Move validation to Join Us.

--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
-import { JoinUsSection } from './join-us.js';
-import { SectionCreatorFactory } from './join-us.js';
-import { validate } from './email-validator.js';
 import './styles/style.css';
+import { SectionCreatorFactory } from './join-us.js';
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   const sectionCreator = new SectionCreatorFactory();
 
-  JoinUsSection.render();
-
-  const emailInput = document.getElementById('email');
-  const email = emailInput.value.trim();
-
-  // Validate the email address using the validate function
-  const isValidEmail = validate(email);
-
-  // Display an alert message with the result of the validation
-  if (isValidEmail) {
-    alert('Email address is valid.');
-  } else {
-    alert('Please enter a valid email address.');
-  }
+  const section = sectionCreator.createSection('standard');
+  section.render();
 });
